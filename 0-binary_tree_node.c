a6280e96459af23e345b387456015e413a64553e
jessyovia <jessyovia100@gmail.com>
2024-02-29 21:11:31 -0800

    0-binary_tree_node.c

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -10,19 +10,19 @@
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    /* Allocate memory for new node */
-    binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
-    
-    /* Check if memory allocation was successful */
-    if (new_node == NULL)
-        return (NULL);
-    
-    /* Initialize node values */
-    new_node->n = value;
-    new_node->parent = parent;
-    new_node->left = NULL;
-    new_node->right = NULL;
-    
-    /* Return pointer to new node */
-    return (new_node);
+	/* Allocate memory for new node */
+	binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
+
+	/* Check if memory allocation was successful */
+	if (new_node == NULL)
+		return (NULL);
+
+	/* Initialize node values */
+	new_node->n = value;
+	new_node->parent = parent;
+	new_node->left = NULL;
+	new_node->right = NULL;
+
+	/* Return pointer to new node */
+	return (new_node);
 }
